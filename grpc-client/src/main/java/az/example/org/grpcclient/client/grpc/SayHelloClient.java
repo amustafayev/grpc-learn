113e4918e046c0d3aa043410aff0ea6f3b174434
amustafayev <afganmustafayev99@gmail.com>
2024-07-18 17:54:03 +0400

    Added client impl(not finished yet)

--- grpc-client/src/main/java/az/example/org/grpcclient/client/grpc/SayHelloClient.java
+++ grpc-client/src/main/java/az/example/org/grpcclient/client/grpc/SayHelloClient.java
@@ -1,6 +1,59 @@
 package az.example.org.grpcclient.client.grpc;
 
+import com.example.grpc.HelloResponse;
+import com.example.grpc.MyHelloServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SayHelloClient {
 
+    @GrpcClient("myHelloService")
+    private MyHelloServiceGrpc.MyHelloServiceBlockingStub myServiceStub;
+//    @GrpcClient("myHelloService")
+//    private MyHelloServiceGrpc.MyHelloServiceStub myServiceStub;
 
+    @EventListener(ContextRefreshedEvent.class)
+    public void callSayHelloServer() {
+//
+//        StreamObserver<HelloResponse> responseObserver = new StreamObserver<HelloResponse>() {
+//
+//            @Override
+//            public void onNext(HelloResponse helloResponse) {
+//                System.out.println(helloResponse.getMessage());
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//                System.out.println("errror");
+//            }
+//
+//            @Override
+//            public void onCompleted() {
+//                System.out.println("Done client!");
+//            }
+//        };
+//
+//        com.example.grpc.HelloRequest request = com.example.grpc.HelloRequest
+//                .newBuilder()
+//                .setName("Afgan")
+//                .build();
+//
+//        myServiceStub.sayHello(request, responseObserver);
+
+
+        System.out.println("Client working...");
+
+        com.example.grpc.HelloRequest request = com.example.grpc.HelloRequest
+                .newBuilder()
+                .setName("Afgan")
+                .build();
+
+        HelloResponse helloResponse = myServiceStub.sayHello(request);
+
+        System.out.println(helloResponse.getMessage());
+
+    }
 }
---
+++ grpc-client/src/main/java/az/example/org/grpcclient/controller/SayHelloStubController.java
@@ -0,0 +1,23 @@
+package az.example.org.grpcclient.controller;
+
+import az.example.org.grpcclient.client.grpc.SayHelloClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/grpc")
+public class SayHelloStubController {
+
+    private final SayHelloClient sayHelloClient;
+
+    public SayHelloStubController(SayHelloClient sayHelloClient) {
+        this.sayHelloClient = sayHelloClient;
+    }
+
+    @GetMapping
+    public void sayHello() {
+        sayHelloClient.callSayHelloServer();
+    }
+
+}
